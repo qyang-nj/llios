658f8e55cb33c947752badf8bb999f76143470ce
Qing Yang <yangq.nj@gmail.com>
2021-04-13 09:30:00 -0700

    add static lib for dce comparision

--- dce/build.sh
+++ dce/build.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+set -e
+
+[ -z "$DEAD_STRIP" ] && DEAD_STRIP=1
+[ -z "$USE_STATIC_LIB" ] && USE_STATIC_LIB=0
 
 # Swift symbols are preserved by default for relection, so we need to disable reflection
 xcrun swiftc -parse-as-library -c -o lib.o -Xfrontend -disable-reflection-metadata lib.swift
+libtool -static lib.o -o lib.a
+
 xcrun swiftc -c -Xfrontend -disable-reflection-metadata -o main.o main.swift
+
+[ "$DEAD_STRIP" == 1 ] && dead_strip_flag="-dead_strip" || dead_strip_flag=""
+[ "$USE_STATIC_LIB" == 1 ] && lib_bin=lib.a || lib_bin=lib.o
 
 # We can toggle `-dead_strip` to see what can be stripped.
 xcrun ld -syslibroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk \
     -L /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX11.1.sdk/usr/lib/swift \
     -lobjc -lSystem \
-    -dead_strip \
-    lib.o main.o
-
+    $dead_strip_flag \
+    $lib_bin main.o
 
 # Verify what symbols are no_dead_strip
-nm -nm lib.o | grep "no dead strip"
+# nm -nm lib.o | grep "no dead strip"
 
-# Observe the strings are preserved. Others are stripped.
+# Observe the preserved strings. Others are stripped.
 strings a.out
--- dce/lib.swift
+++ dce/lib.swift
@@ -1,13 +1,13 @@
 // Public symbols are always no_dead_strip.
 public struct PublicStruct {
     public func f1() {
-        print("public print")
+        print("print public")
     }
 }
 
 // Non-public sysmbols can be stripped by linker if unused.
 struct InternalStruct {
     func f2() {
-        print("internal print")
+        print("print internal")
     }
 }
