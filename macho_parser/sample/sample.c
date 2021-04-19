db8e67a0ca75b1c9210275471b3cf02f8779cb72
Qing Yang <yangq.nj@gmail.com>
2021-04-18 20:55:42 -0700

    add an exmaple for __atribute__((used))

--- macho_parser/sample/sample.c
+++ macho_parser/sample/sample.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 
+// Function with "constructor" attribute will be added to __DATA,__mod_init_func section
 __attribute__((constructor)) void c_constructor_function() {}
+
+// Symbols with "used" attribute will be marked as no dead strip in the objective file.
+__attribute__((used)) static void c_used_function() {}
 
 int main()
 {
+    // String literal will be added to __TEXT,__cstring section
     printf("hello, world!\n");
     return 0;
 }
