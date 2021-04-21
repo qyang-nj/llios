bea8cc8ff078d9854bd16b4e0618385bee29f6ff
Qing Yang <yangq.nj@gmail.com>
2021-04-21 15:09:25 -0700

    update marcho_parser/README.md and format

--- macho_parser/README.md
+++ macho_parser/README.md
@@ -17,7 +17,8 @@ cd sample && ./build.sh && cd ..
 ```
 
 ## Notes
-### __mod_init_func
+### LC_SEGMENT_64
+#### __mod_init_func
 `(__DATA,__mod_init_func)` or `(__DATA_CONST,__mod_init_func)`
 
 This is the section that contains of a list of function pointers, which will [be executed by `dyld`](https://github.com/opensource-apple/dyld/blob/3f928f32597888c5eac6003b9199d972d49857b5/src/ImageLoaderMachO.cpp#L1815~L1847) before `main`. Those are functions with `__attribute__((constructor))` and they will affect the app launch time.
@@ -28,17 +29,36 @@ Once we have the function pointer address, we can use `atos` to query the functi
 c_constructor_function (in sample) + 0
 ```
 
-> Please note that ObjC's `+load` methods will also be executed before `main`, but uses a different mechanism. See below "+load in ObjC" section.
+⚠️ Please note that ObjC's `+load` methods will also be executed before `main`, but uses a different mechanism. See below "+load in ObjC" section.
 
-### `+load` in ObjC
+### LC_SYMTAB
+#### Two Level Namespace
+The linker enables the two-level namespace option (`-twolevel_namespace`) by default. It can be disabled by `-flat_namespace` option. The two levels are the library name and symbol name. Once enabled, the macho header has `MH_TWOLEVEL` flag set. Each undefined symbols will record its library information by `LIBRARY_ORDINAL` in `nlist.n_desc`.
+
+Two major benefits of two-level namespace:
+* avoid symbol conflict from different libraries
+* accelerate symbol lookup at runtime
+
+##### References
+[Mac OS X Developer Release Notes: Two-Level Namespace Executables]()
+
+#### N_NO_DEAD_STRIP
+Enabled by `__attribute__((constructor))`. It tells the linker (`ld`) to keep this symbol even it's not used. It's a flag in `nlist.n_desc` and exits in object of instead file executable.
+
+#### N_WEAK_REF
+Enabled by `__attribute__((weak))`. It tells dynamic loader (`dyld`) if the symbol cannot be found at runtime, set NULL to its address.
+
+
+### Other
+#### `+load` in ObjC
 The MachO that has Objective-C code will have two sections, `(__DATA_CONST,__objc_classlist)` and `(__DATA_CONST,__objc_nlclslist)`. `__objc_classlist` includes the addresses of all ObjC classes, while `__objc_nlclslist` contains only *non-lazy* classes. [Non-lazy classes are classes that have `+load` method](https://github.com/opensource-apple/objc4/blob/cd5e62a5597ea7a31dccef089317abb3a661c154/runtime/objc-runtime-new.mm#L2806~L2812) and will be loaded at launch time.
 
-#### How `+load` is executed during startup?
+**How `+load` is executed during startup?**
 1. dyld calls [_objc_init](https://github.com/opensource-apple/objc4/blob/cd5e62a5597ea7a31dccef089317abb3a661c154/runtime/objc-os.mm#L803~L831), where [a notification is registered when an image is loaded](https://github.com/opensource-apple/objc4/blob/cd5e62a5597ea7a31dccef089317abb3a661c154/runtime/objc-os.mm#L830).
 2. In the notification callback, [load_images](https://github.com/opensource-apple/objc4/blob/cd5e62a5597ea7a31dccef089317abb3a661c154/runtime/objc-runtime-new.mm#L2157~L2193), it [calls the load methods](https://github.com/opensource-apple/objc4/blob/cd5e62a5597ea7a31dccef089317abb3a661c154/runtime/objc-loadmethod.mm#L306~L365) in that image.
 
 The difference between `+load` and `__mod_init_func` is that the former guarantees [certain order of execution](https://developer.apple.com/documentation/objectivec/nsobject/1418815-load?language=objc), while the latter doesn't.
 
-#### References
+##### References
 * [Objective-C: What is a lazy class?](https://stackoverflow.com/a/15318325/3056242)
 * [What did Runtime do during the startup of Runtime objc4-779.1 App?](https://programmer.group/what-did-runtime-do-during-the-startup-of-runtime-objc4-779.1-app.html)
--- macho_parser/main.c
+++ macho_parser/main.c
@@ -20,10 +20,8 @@ void *load_bytes(FILE *fptr, int offset, int size) {
     return buf;
 }
 
-int main(int argc, char **argv)
-{
-    if (argc < 2)
-    {
+int main(int argc, char **argv) {
+    if (argc < 2) {
         puts("Error: missing Mach-O file.");
         return 1;
     }
@@ -37,21 +35,17 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void parse_load_commands(FILE *fptr, int offset, uint32_t ncmds)
-{
-    for (int i = 0; i < ncmds; ++i)
-    {
+void parse_load_commands(FILE *fptr, int offset, uint32_t ncmds) {
+    for (int i = 0; i < ncmds; ++i) {
         struct load_command *lcmd = load_bytes(fptr, offset, sizeof(struct load_command));
 
-        if (lcmd->cmd == LC_SEGMENT_64)
-        {
+        if (lcmd->cmd == LC_SEGMENT_64) {
             struct segment_command_64 *seg_cmd = load_bytes(fptr, offset, lcmd->cmdsize);
             printf("LC_SEGMENT_64: %s (%lld)\n", seg_cmd->segname, seg_cmd->filesize);
             parse_sections(fptr, seg_cmd);
             free(seg_cmd);
         }
-        else if (lcmd->cmd == LC_SYMTAB)
-        {
+        else if (lcmd->cmd == LC_SYMTAB) {
             struct symtab_command *cmd = load_bytes(fptr, offset, lcmd->cmdsize);
             printf("LC_SYMTAB (symtab: %lu, strtab: %u)\n", cmd->nsyms * sizeof(struct nlist_64), cmd->strsize);
             parse_symbol_table(fptr, cmd);
@@ -63,38 +57,32 @@ void parse_load_commands(FILE *fptr, int offset, uint32_t ncmds)
     }
 }
 
-void parse_sections(FILE *fptr, struct segment_command_64 *seg_cmd)
-{
+void parse_sections(FILE *fptr, struct segment_command_64 *seg_cmd) {
     // section_64 is immediately after segment_command_64.
     struct section_64 *sections = (void *)seg_cmd + sizeof(struct segment_command_64);
 
-    for (int i = 0; i < seg_cmd->nsects; ++i)
-    {
+    for (int i = 0; i < seg_cmd->nsects; ++i) {
         struct section_64 sect = sections[i];
         printf("    (%s,%s) [size: %lld]\n", sect.segname, sect.sectname, sect.size);
 
         const uint8_t type = sect.flags & SECTION_TYPE;
 
         // (__TEXT,__cstring), (__TEXT,__objc_classname__TEXT), (__TEXT,__objc_methname), etc..
-        if (type == S_CSTRING_LITERALS) // __cstring
-        {
+        if (type == S_CSTRING_LITERALS) {
             parse_cstring_section(fptr, &sect);
         }
         // (__DATA_CONST,__mod_init_func)
-        else if (type == S_MOD_INIT_FUNC_POINTERS)
-        {
+        else if (type == S_MOD_INIT_FUNC_POINTERS) {
             parse_mod_init_func_section(fptr, &sect);
         }
     }
 }
 
-void parse_cstring_section(FILE *fptr, struct section_64 *cstring_sect)
-{
+void parse_cstring_section(FILE *fptr, struct section_64 *cstring_sect) {
     void *section = load_bytes(fptr, cstring_sect->offset, cstring_sect->size);
 
     char formatted[256];
-    for (char *ptr = section; ptr < (char *)(section + cstring_sect->size);)
-    {
+    for (char *ptr = section; ptr < (char *)(section + cstring_sect->size);) {
         if (strlen(ptr) > 0) {
             format_string(ptr, formatted);
             printf("        \"%s\"\n", formatted);
@@ -106,8 +94,7 @@ void parse_cstring_section(FILE *fptr, struct section_64 *cstring_sect)
     free(section);
 }
 
-void parse_mod_init_func_section(FILE *fptr, struct section_64 *sect)
-{
+void parse_mod_init_func_section(FILE *fptr, struct section_64 *sect) {
     void *section = load_bytes(fptr, sect->offset, sect->size);
 
     const size_t count = sect->size / sizeof(uintptr_t);
@@ -118,18 +105,15 @@ void parse_mod_init_func_section(FILE *fptr, struct section_64 *sect)
     free(section);
 }
 
-void parse_symbol_table(FILE *fptr, struct symtab_command *sym_cmd)
-{
+void parse_symbol_table(FILE *fptr, struct symtab_command *sym_cmd) {
     void *sym_table = load_bytes(fptr, sym_cmd->symoff, sym_cmd->nsyms * sizeof(struct nlist_64));
     void *str_table = load_bytes(fptr, sym_cmd->stroff, sym_cmd->strsize);
 
     char formatted_n_desc[256];
 
-    for (int i = 0; i < sym_cmd->nsyms; ++i)
-    {
+    for (int i = 0; i < sym_cmd->nsyms; ++i) {
         struct nlist_64 *nlist = sym_table + sizeof(struct nlist_64) * i;
         char *symbol = str_table + nlist->n_un.n_strx;
-        char *no_dead_strip = (nlist->n_desc & N_NO_DEAD_STRIP) ? " [no dead strip]" : "";
 
         format_n_desc(nlist->n_desc, formatted_n_desc);
 
@@ -167,13 +151,10 @@ void format_n_desc(uint16_t n_desc, char *formatted) {
 }
 
 // If the string contains '\n', replace with literal "\n".s
-void format_string(char *str, char *formatted)
-{
+void format_string(char *str, char *formatted) {
     int j = 0;
-    for (int i = 0; str[i] != '\0'; ++i)
-    {
-        switch(str[i])
-        {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        switch(str[i]) {
             case '\n':
                 formatted[j++] = '\\';
                 formatted[j++] = 'n';
