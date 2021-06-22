4ea9e990cce2469f279272b7acc25d6594f3b68c
Qing Yang <yangq.nj@gmail.com>
2021-06-22 16:27:41 -0700

    move to source dir

--- macho_parser/build.sh
+++ macho_parser/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-clang -o parser main.c argument.c util.c segment_64.c \
-    symtab.c dysymtab.c dyld_info.c linkedit_data.c build_version.c
+srcs=$(ls sources/*.c)
+clang -o parser $srcs
