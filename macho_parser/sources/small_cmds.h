d6be97fdea80d36f00687b9501f7138647733b8b
Qing Yang <yangq.nj@gmail.com>
2022-01-03 14:18:27 -0800

    add small_cmd.cpp

---
+++ macho_parser/sources/small_cmds.h
@@ -0,0 +1,13 @@
+#ifndef SMALL_CMDS_H
+#define SMALL_CMDS_H
+
+#include <mach-o/loader.h>
+
+void printDyLinker(void *base, struct dylinker_command *);
+void printEntryPoint(void *base, struct entry_point_command *);
+void printLinkerOption(void *base, struct linker_option_command *);
+void printRpath(void *base, struct rpath_command *);
+void printUUID(void *base, struct uuid_command *cmd);
+void printSourceVersion(void *base, struct source_version_command *cmd);
+
+#endif /* SMALL_CMDS_H */
