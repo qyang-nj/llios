5c3fd3d5dd6c557f4f2b187bebf92095b86d98d0
Qing Yang <yangq.nj@gmail.com>
2022-01-07 16:47:14 -0800

    update open source version

--- apple_open_source/download.sh
+++ apple_open_source/download.sh
@@ -9,8 +9,8 @@ fi
 
 CCTOOLS_URL="https://opensource.apple.com/tarballs/cctools/cctools-973.0.1.tar.gz"
 LD64_URL="https://opensource.apple.com/tarballs/ld64/ld64-609.tar.gz"
-DYLD_URL="https://opensource.apple.com/tarballs/dyld/dyld-851.27.tar.gz"
-XNU_URL="https://opensource.apple.com/tarballs/xnu/xnu-7195.101.1.tar.gz"
+DYLD_URL="https://opensource.apple.com/tarballs/dyld/dyld-852.2.tar.gz"
+XNU_URL="https://opensource.apple.com/tarballs/xnu/xnu-7195.141.2.tar.gz"
 LIB_SECURITY_CODESIGNING="https://opensource.apple.com/tarballs/libsecurity_codesigning/libsecurity_codesigning-55037.15.tar.gz"
 
 for url in "$CCTOOLS_URL" "$LD64_URL" "$DYLD_URL" "$XNU_URL" "$LIB_SECURITY_CODESIGNING"; do
