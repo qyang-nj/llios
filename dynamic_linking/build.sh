7bacab34730163684c66c5be027bcb4e787ba652
Qing Yang <yangq.nj@gmail.com>
2022-04-04 17:11:44 -0700

    fix

--- dynamic_linking/build.sh
+++ dynamic_linking/build.sh
@@ -4,7 +4,7 @@
 
 [ "$FIXUP_CHAINS" == 1 ] && fixup_chains_flag="-Xlinker -fixup_chains" || fixup_chains_flag=""
 
-# Make sure we're build for x86_64 arch, because the article is based on x86_64 assembly.
+# Make sure it's built for x86_64 arch, because the article is based on x86_64 assembly.
 target="x86_64-apple-macos"
 
 clang -target $target -dynamiclib -o build/lib.dylib lib.c
