8cf12809510184e4740b9b42b27bd76e6bcbc78a
Qing Yang <qing.yang@airbnb.com>
2023-05-14 20:20:06 -0700

    leb128

--- macho_parser/sources/util.c
+++ macho_parser/sources/util.c
@@ -2,40 +2,6 @@
 
 #include "util.h"
 
-int read_uleb128(const uint8_t *p, uint64_t *out) {
-    uint64_t result = 0;
-    int i = 0;
-
-    do {
-        uint8_t byte = *p & 0x7f;
-        result |= (uint64_t)byte << (i * 7);
-        i++;
-    } while (*p++ & 0x80);
-
-    *out = result;
-    return i;
-}
-
-int read_sleb128(const uint8_t *p, int64_t *out) {
-    int64_t result = 0;
-    int i = 0;
-    uint8_t byte;
-
-    do {
-        byte = *p & 0x7f;
-        result |= byte << (i * 7);
-        i++;
-    } while (*p++ & 0x80);
-
-    // The sign bit is set
-    if (byte & 0x40) {
-        result |= (~0 << (i * 7));
-    }
-
-    *out = result;
-    return i;
-}
-
 void format_string(char *str, char *formatted) {
     int j = 0;
     for (int i = 0; str[i] != '\0'; ++i) {
